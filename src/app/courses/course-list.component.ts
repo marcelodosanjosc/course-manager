17c88f13357a16cab4d886670822112b7deb1777
marcelodosanjosc <marcelodosanjosc@gmail.com>
2021-07-12 10:21:44 -0400

    Adicionado classes de service no courses

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -8,29 +9,10 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit{
     
     courses: Course[] = [];
+
+    constructor(private courseService: CourseService){ }
     
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name:'Angular: Forms ',
-                imageUrl:'',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration:120,
-                rating:5.4,
-                releaseDate:'12/02/2020'
-            },
-            {
-                id: 2,
-                name:'Angular: HTTP ',
-                imageUrl:'',
-                price: 45.99,
-                code: 'LXL-1894',
-                duration:80,
-                rating:4,
-                releaseDate:'12/04/2020'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
     }
 }
